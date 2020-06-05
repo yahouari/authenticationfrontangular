b2810caa5b2a1c775227f2bebea461edc8308791
YoussefChana <59029575+YoussefChana@users.noreply.github.com>
2020-06-05 14:59:40 +0100

    Add files via upload

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    ideval: number;
+    username: string;
+    destination: string;
+    note: number;
+    dateevaluation: string;
+    constructor(
+        ideval: number= 586,
+        username: string= '',
+        destination: string= '',
+        note: number= 0,
+        dateevaluation: string= '2020-05-02',
+    ) {}
+}
